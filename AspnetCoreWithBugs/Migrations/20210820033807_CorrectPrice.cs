8ebe3607d818da642b4c28a9aaa13b07cf74bcc2
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-19 20:42:12 -0700

    Issue#2 create database (#7)
    * Created Initial database migration using Add-Migration
    
    * Changed price in product model to double instead of string
    
    * Updated database to store Price as a double

---
+++ AspnetCoreWithBugs/Migrations/20210820033807_CorrectPrice.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AspnetCoreWithBugs.Migrations
+{
+    public partial class CorrectPrice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Product",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Product",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}

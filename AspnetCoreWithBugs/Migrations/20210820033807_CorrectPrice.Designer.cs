a0e01cbb33e6469ce1a73d0b73e1d71babc41b0a
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-08-19 20:40:06 -0700

    Updated database to store Price as a double

--- AspnetCoreWithBugs/Migrations/20210820033807_CorrectPrice.Designer.cs
+++ AspnetCoreWithBugs/Migrations/20210820033807_CorrectPrice.Designer.cs
@@ -3,14 +3,16 @@ using AspnetCoreWithBugs.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AspnetCoreWithBugs.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20210820033807_CorrectPrice")]
+    partial class CorrectPrice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
